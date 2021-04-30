f99897486f472c8f2e60eea4c7cfef6ea43566a6
Aman saxena <asaxena7531@gmail.com>
2021-04-30 17:28:36 +0530

    adding ChooserActivity where user can choose passgen and notes activity.

---
+++ app/src/main/java/com/example/firenotes/ChooserActivity.java
@@ -0,0 +1,26 @@
+package com.example.firenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooserActivity extends AppCompatActivity {
+    Button passGenBtn;
+    Button NotesBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chooser);
+        passGenBtn = findViewById(R.id.PasswordGeneratorBtn);
+        NotesBtn = findViewById(R.id.NotesBtn);
+        NotesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+}
